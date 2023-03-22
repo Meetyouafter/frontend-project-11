c144e3567e72cb847df8a02c155288dcc2abdfe1
Levis <anton_levus@mail.ru>
2023-03-22 20:35:27 +0530

    refactoring

--- src/view/modalView.js
+++ src/view/modalView.js
@@ -1,15 +1,11 @@
 const modalView = (elements, state) => {
-  const { body } = elements;
   const {
-    modalContainer, title, description, readBtn,
+    title, description, readBtn,
   } = elements.modal;
   const id = state.uiState.openPostId;
   const openPost = state.posts.find((post) => post.idItem === id);
 
-  const handleOpenClick = (post) => {
-    // body.classList.add('modal-open');
-    // modalContainer.classList.add('show');
-    // modalContainer.style.display = 'block';
+  const handleOpen = (post) => {
     readBtn.href = post.link;
     title.textContent = post.title;
     description.textContent = post.description;
@@ -18,7 +14,7 @@ const modalView = (elements, state) => {
   const currentLink = document.querySelector(`a[data-id="${id}"]`);
   currentLink.classList.remove('fw-bold');
   currentLink.classList.add('fw-normal', 'link-secondary');
-  handleOpenClick(openPost);
+  handleOpen(openPost);
 };
 
 export default modalView;
