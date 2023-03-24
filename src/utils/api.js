32dc4141547a743d0487cba74f6cb6604fac3980
Levis <anton_levus@mail.ru>
2023-03-24 10:50:07 +0530

    fixed proxy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,16 @@
 const getFeedsLinks = (state) => state.feeds.map((feed) => feed.linkName);
 
-const proxy = (link) => {
+const proxy = (url) => {
+  const proxyUrl = new URL('/get', 'https://allorigins.hexlet.app');
+  proxyUrl.searchParams.append('disableCache', 'true');
+  proxyUrl.searchParams.append('url', url);
+  return proxyUrl;
+};
+  /*
+  console.log({link});
   const url = new URL(link);
+  console.log({url});
   return `https://allorigins.hexlet.app/get?disableCache=false&url=${encodeURIComponent(url)}`;
 };
-
+*/
 export { getFeedsLinks, proxy };
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -23,5 +23,35 @@ const validate = (state, i18Instance) => {
 
   return validateSchema;
 };
+/*
 
+import * as yup from 'yup';
+import { getFeedsLinks } from './api.js';
+
+const validate = (state, i18Instance) => {
+  const feedsLinks = getFeedsLinks(state);
+  const schema = yup.object({
+    url: yup.string()
+      .required()
+      .url()
+      .notOneOf(feedsLinks, i18Instance.t('form.rssExist')),
+  });
+
+  const validateSchema = (link) => schema
+    .validate({ url: link }, { abortEarly: false })
+    .then(({ url }) => {
+      state.form.errors = {};
+
+      return Promise.resolve(url.trim());
+    })
+    .catch((err) => {
+      throw err;
+    });
+
+  return validateSchema;
+};
+
+export default validate;
+
+*/
 export default validate;
