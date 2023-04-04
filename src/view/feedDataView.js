a8c273a2aca642569a16457d6b28456f0c0e8ecb
Meetyouafter <anton_levus@mail.ru>
2023-04-04 17:02:16 +0530

    added DOMPurify

--- src/view/feedDataView.js
+++ src/view/feedDataView.js
@@ -1,35 +1,37 @@
+import DOMPurify from 'dompurify';
+
 const feedHtml = (data, i18next, language) => (`
-    <div class="card border-0">
-      <div class="card-body">
-        <h2 class="card-title feeds-title h4">${i18next.t('interface.feedsTitle', { lng: language })}</h2>
-      </div>
-      <ul class="list-group border-0 rounded-0">
-        ${data.map((item) => (`
-          <li class="list-group-item border-0 border-end-0">
-            <h3 class="h6 m-0">${item.feedTitle}</h3>
-            <p class="m-0 small text-black-50">${item.feedDescription}</p>
-          </li>`)).join('')}
-      </ul>
-    </div>`);
+  <div class="card border-0">
+    <div class="card-body">
+      <h2 class="card-title feeds-title h4">${i18next.t('interface.feedsTitle', { lng: language })}</h2>
+    </div>
+    <ul class="list-group border-0 rounded-0">
+      ${data.map((item) => (`
+        <li class="list-group-item border-0 border-end-0">
+          <h3 class="h6 m-0">${DOMPurify.sanitize(item.feedTitle)}</h3>
+          <p class="m-0 small text-black-50">${DOMPurify.sanitize(item.feedDescription)}</p>
+        </li>`)).join('')}
+    </ul>
+  </div>`);
 
 const postHtml = (data, i18next, language) => (`
-    <div class="card border-0">
-      <div class="card-body">
-        <h2 class="card-title posts-title h4">${i18next.t('interface.postsTitle', { lng: language })}</h2>
-      </div>
-      <ul class="list-group border-0 rounded-0">
-        ${data.map((post) => (`
-          <li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
-            <a href="${post.link}" class="fw-bold" data-id="${post.idItem}" target="_blank" rel="noopener noreferrer">
-              ${post.title}
-            </a>
-            <button type="button" class="btn btn-outline-primary btn-sm" data-id="${post.idItem}" data-bs-toggle="modal" data-bs-target="#modal">
-              Просмотр
-            </button>
-          </li>
-        `)).join('')}
-      </ul>
-    </div>`);
+  <div class="card border-0">
+    <div class="card-body">
+      <h2 class="card-title posts-title h4">${i18next.t('interface.postsTitle', { lng: language })}</h2>
+    </div>
+    <ul class="list-group border-0 rounded-0">
+      ${data.map((post) => (`
+        <li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+          <a href="${DOMPurify.sanitize(post.link)}" class="fw-bold" data-id="${post.idItem}" target="_blank" rel="noopener noreferrer">
+            ${DOMPurify.sanitize(post.title)}
+          </a>
+          <button type="button" class="btn btn-outline-primary btn-sm" data-id="${post.idItem}" data-bs-toggle="modal" data-bs-target="#modal">
+            Просмотр
+          </button>
+        </li>
+      `)).join('')}
+    </ul>
+  </div>`);
 
 const feedDataView = (elements, state, i18next) => {
   const { feeds, posts } = elements;
